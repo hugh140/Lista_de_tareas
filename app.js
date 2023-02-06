8ffdaa9fa8aa365cace1fc363bd69a49a20a1c4c
Hugo Lap <hugofer300@gmail.com>
2023-02-06 18:29:16 -0500

    Implementado la función de agregar tareas

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ import express from "express";
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+
+let tareas = []
 
 app.get('/', (req, res) => {
-    res.render('lista.ejs', {vari: 'Hugo'})
+    res.render('lista', {tareasRegistradas: tareas})
 })
 
-app.listen(3000, '192.168.100.13', () => {
+app.post('/', (req, res) => {
+    tareas.push(req.body.tareaFormulario)
+    console.log(tareas)
+    res.redirect('/')
+})
+
+app.listen(3000, () => {
     console.log('Conectado en el puerto', 3000)
 })
